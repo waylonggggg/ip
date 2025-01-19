f1eb58d10b082282d799486b39046264a0643c4e
Waylon <tanwaylon38@gmail.com>
2025-01-20 01:34:27 +0800

    Add list functionality

--- src/main/java/Sonder.java
+++ src/main/java/Sonder.java
@@ -1,6 +1,9 @@
 import java.util.Scanner;
 
 public class Sonder {
+    private static String[] list = new String[100];
+    private static int index = 0;
+
     public static void main(String[] args) {
         String message = "____________________________________________________________\n"
                 + "Hello! I'm Sonder!\n"
@@ -10,19 +13,25 @@ public class Sonder {
 
         Scanner sc = new Scanner(System.in);
 
-        // Echo Message
+        // List inputs
         while (true) {
-            String echo = sc.nextLine();
+            String input = sc.nextLine();
 
-            if (echo.equalsIgnoreCase("bye")) {
+            if (input.equalsIgnoreCase("bye")) {
                 System.out.println(
                         "____________________________________________________________\n"
                                 + "Bye. Hope to see you again soon!\n"
                                 + "____________________________________________________________"
                 );
                 break;
+            } else if (input.equalsIgnoreCase("list")) {
+                for (int i = 0; i < index; i++) {
+                    System.out.println(String.format("%d." + list[i], i + 1));
+                }
             } else {
-                System.out.println(echo);
+                System.out.println("added: " + input);
+                list[index] = input;
+                index++;
             }
         }
     }
